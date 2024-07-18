ed76411d58bec6c28cc4dae8e9be5ca7ad14c86b
11310 <11310@AIR>
2024-07-18 15:14:49 +0800

    Add project files.

---
+++ 5032_2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using _5032_2.Models.HelloWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _5032_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            // I commented this line out
+            //ViewBag.Message = "Your application description page.";
+            // I created an instance of the Hello object
+            Hello hello = new Hello();
+            // I assigned the ViewBag.Message to the result
+            ViewBag.Message = hello.GetHello();
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ 5032_2/Models/HelloWorld/Hello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _5032_2.Models.HelloWorld
+{
+    public class Hello
+    {
+        public String GetHello()
+        {
+            return "Hello World!";
+        }
+    }
+}
